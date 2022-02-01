4c85dd770772b049bdb13f56fc6f18240f4f4dce
Gunther <grldsnunes@inf.ufpel.edu.br>
2022-02-01 13:22:06 -0300

    Changed import path

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:cat_summon_circle_flutter/routes/summoning_circle.dart';
+import 'package:cat_summon_circle_flutter/src/summoning_circle.dart';
 import 'package:flutter/material.dart';
 
 void main() {
